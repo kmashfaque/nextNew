4a665bab0046e4ba6e917a8bfd9892f024966b49
Ashfaque <nvjkvhdjkh@gmail.com>
2021-12-22 16:31:12 +0600

    second commit

--- projectone/pages/events/[eventId].js
+++ projectone/pages/events/[eventId].js
@@ -1,8 +1,30 @@
-function EventDetails(){
-    return(
-        <>
-        <h1>Events Details</h1>
-        </>
-    )
+import { getEventById } from "../../dummy-data";
+import { useRouter } from "next/router";
+import EventContent from "../../components/event-detail/event-content";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventSummary from "../../components/event-detail/event-summary";
+
+function EventDetails() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found</p>;
+  }
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 }
-export default EventDetails
+export default EventDetails;
--- projectone/pages/events/index.js
+++ projectone/pages/events/index.js
@@ -1,8 +1,15 @@
-function AllEventsPage(){
-    return(
-        <>
-        <h1>All Events</h1>
-        </>
-    )
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import { Fragment } from "react";
+
+function AllEventsPage() {
+  const events = getAllEvents();
+  return (
+    <Fragment>
+      <EventsSearch />
+      <EventList items={events} />
+    </Fragment>
+  );
 }
-export default AllEventsPage
+export default AllEventsPage;
