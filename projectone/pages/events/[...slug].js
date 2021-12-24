6e9383c49c9d753f9e4f9100c75bf10c88b88463
Ashfaque <nvjkvhdjkh@gmail.com>
2021-12-24 22:47:07 +0600

    fourth commit

--- projectone/pages/events/[...slug].js
+++ projectone/pages/events/[...slug].js
@@ -1,41 +1,46 @@
-import { useRouter } from "next/router"
-import {getFilteredEvents} from "../../dummy-data"
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+
 function FilteredEvents() {
-    
-    const router = useRouter()
-    
-    const filterData=router.query.slug
+  const router = useRouter();
 
-    if(!filterData){
-        return<p>Loading....</p>
-    }
+  const filterData = router.query.slug;
 
-    const filteredYear=filterData[0]
-    const filterdMonth = filterData[1]
-    
-    const numYear = +filteredYear
-    const numMonth= +filterdMonth
+  if (!filterData) {
+    return <p>Loading....</p>;
+  }
 
+  const filteredYear = filterData[0];
+  const filterdMonth = filterData[1];
 
-    if (isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 2021 || numMonth < 1 || numMonth > 12) {
-        return <p>Invalid filter. Please adjust your values</p>
-    }
+  const numYear = +filteredYear;
+  const numMonth = +filterdMonth;
 
-    const filteredEvents=getFilteredEvents({
-        year:numYear,
-        month:numMonth
-    })
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values</p>;
+  }
 
-    if(!filteredEvents||filteredEvents.length===0){
-        return <p> No events founds for the chosen filter!!</p>
-    }
-    return(
-        <>
-        <h1>
-            Filtered Events
-        </h1>
-        </>
-    )
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p> No events founds for the chosen filter!!</p>;
+  }
+  return (
+    <>
+      <EventList items={filteredEvents} />
+    </>
+  );
 }
 
-export default FilteredEvents
+export default FilteredEvents;
